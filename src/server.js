6436d832fe93390765e50dc4bcd8a3b35cbf4c49
Deniz Burduroglu <denizburduroglu@gmail.com>
2021-05-04 10:16:36 -0400

    fixing possible port issues

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 // Install express server
+const PORT = process.env.PORT || '8080';
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
 app.use(express.static(__dirname + '/dist/sauce-boss'));
@@ -10,4 +10,6 @@ app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname+'/dist/sauce-boss/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+
+app.set('port', PORT)
+app.listen(PORT);
